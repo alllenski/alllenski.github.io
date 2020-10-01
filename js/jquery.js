d3144f7483a0e42d6c8518cf6f479387c89a98c6
alllenski <allenrada21@gmail.com>
2020-10-01 23:03:46 +0800

    Added to gallery, changed logo, added memory thingy

--- js/jquery.js
+++ js/jquery.js
@@ -3,4 +3,16 @@ $(document).ready(function(){
    
     $('#content-header').load("common/navbar.md");
 
+   
+
+});
+
+$(document).ajaxStop(function() {
+
+    $('#logo').mouseenter(function(){
+        let randomPositionX = ~Math.floor(Math.random() * 5) * 90;
+        let randomPositionY = ~Math.floor(Math.random() * 2 - 1) * 90;
+        $("#logo").css("background-position", randomPositionX.toString() + "px " + randomPositionY.toString() + "px");
+    });
+
 });
