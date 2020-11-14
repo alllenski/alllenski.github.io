c5f0d3145a8a2293c305a5fe83fd1a9f0dc16d68
alllenski <allenrada21@gmail.com>
2020-11-14 17:41:02 +0800

    Added to gallery

--- js/jquery.js
+++ js/jquery.js
@@ -11,7 +11,7 @@ $(document).ajaxStop(function() {
 
     $('#logo').mouseenter(function(){
         let randomPositionX = ~Math.floor(Math.random() * 5) * 90;
-        let randomPositionY = ~Math.floor(Math.random() * 2 - 1) * 90;
+        let randomPositionY = ~Math.floor(Math.random() * 4 - 1) * 90;
         $("#logo").css("background-position", randomPositionX.toString() + "px " + randomPositionY.toString() + "px");
     });
 
