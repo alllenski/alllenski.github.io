458a1891e81277ea614b93fea40474880c2103b2
alllenski <allenrada21@gmail.com>
2023-06-26 04:13:12 +0800

    Working on redesign / restyle + mobile friendly

--- common/navbar.md
+++ common/navbar.md
@@ -1,5 +1,5 @@
 
-<a href="../index.html"><div id="logo"></div></a>
+<a href="../index.html"><div id="logo"><img src="../img/CatGreenSquare.png"/></div></a>
 <ul>
     <li><a href="../index.html">Home</a></li>
     <li><a href="../works.html">Works</a></li>
--- js/jquery.js
+++ js/jquery.js
@@ -8,11 +8,11 @@ $(document).ready(function(){
 });
 
 $(document).ajaxStop(function() {
-
+    /*
     $('#logo').mouseenter(function(){
         let randomPositionX = ~Math.floor(Math.random() * 5) * 90;
         let randomPositionY = ~Math.floor(Math.random() * 4 - 1) * 90;
         $("#logo").css("background-position", randomPositionX.toString() + "px " + randomPositionY.toString() + "px");
     });
-
+    */
 });
