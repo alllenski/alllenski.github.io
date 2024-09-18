399420379538231d8878c10d5f1441fa8449abf5
alllenski <allenrada21@gmail.com>
2024-09-18 20:49:08 +0800

    a

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
 
     eleventyConfig.addPassthroughCopy("public");
-    eleventyConfig.addPassthroughCopy("**/*.nojekyll");
+
     return {
 		dir: {
 			output: "_docs",
